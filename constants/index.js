a59104494c876bd26da7fb162386149cdfcf9030
Alvin Uday <udayalvin@gmail.com>
2022-06-13 13:17:07 +0530

    add: final commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x968F65EB30d01439a969074252cE6c3b423f9362";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
